e37f6847ec014bb3f2d57c088bfa9948c51c0a7d
Jeffrey Ercolani <Jercolani@JeffyMBP.local>
2015-06-15 18:33:14 -0700

    get CRUD working

--- routes/pieces.js
+++ routes/pieces.js
@@ -4,11 +4,39 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Piece = require('../models/Piece.js');
 
-/* GET /todos listing. */
+/* GET /pieces listing. */
 router.get('/', function(req, res, next) {
   Piece.find(function (err, pieces) {
     if (err) return next(err);
     res.json(pieces);
+  });
+});
+
+router.post('/', function(req, res, next) {
+  Piece.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  Piece.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  Piece.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  Piece.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
   });
 });
 
